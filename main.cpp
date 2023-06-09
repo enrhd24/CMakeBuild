9380e57c68c7e11253ec8f3c4b9371349db22b0f
enrhd24 <enrhd567@gmail.com>
2023-06-09 15:16:43 +0900

    OpenCV flip using cloud

--- main.cpp
+++ main.cpp
@@ -5,14 +5,17 @@ using namespace cv;
 using namespace std;  
   
 int main(int, char**){
-    Mat img, img1, imgd, imgd1;
-    img = imread("../img/family.png",IMREAD_COLOR);
-    resize(img,img1,Size(700,700)); 
-    pyrUp(img1,imgd,Size(img1.cols *2,img1.rows *2));
-    pyrDown(img1,imgd1,Size(img1.cols/2, img1.rows/2));
-    imshow("img",img1);
-    imshow("imgd", imgd);
-    imshow("imgd1",imgd1);
+    Mat cloud, cloud_1, cloud1, cloud2, cloud3;
+    cloud = imread("../img/cloud.jpg",IMREAD_COLOR);
+    resize(cloud,cloud_1,Size(200,200));
+    flip(cloud_1,cloud1,0);
+    flip(cloud_1,cloud2,1);
+    flip(cloud_1,cloud3,-1);
+    imshow("cloud",cloud_1);
+    imshow("cloud1",cloud1);
+    imshow("cloud2",cloud2);
+    imshow("cloud3",cloud3);
+
     waitKey(0);
     return 0;
 }
