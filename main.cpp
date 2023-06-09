f655adec92f1b4eaf03a7fce91758617cff4d867
enrhd24 <enrhd567@gmail.com>
2023-06-09 15:03:20 +0900

    OpenCV Image Up && Dowm

--- main.cpp
+++ main.cpp
@@ -4,32 +4,15 @@
 using namespace cv;  
 using namespace std;  
   
-  
-  
-int main(int, char**)  
-{  
-
-    VideoCapture cap(-1);  
-    if (!cap.isOpened())  
-    {  
-        printf("카메라를 열수 없습니다. \n");  
-    }  
-  
-  
-    Mat frame;  
-    namedWindow("camera1", 1);  
-   
-  
-    for (;;)  
-    {  
-  
-        cap >> frame;  
-  
-        imshow("camera1", frame);  
-  
-        if (waitKey(20) >= 0) break;  
-    }  
-  
-  
-    return 0;  
-}  
+int main(int, char**){
+    Mat img, img1, imgd, imgd1;
+    img = imread("../img/family.png",IMREAD_COLOR);
+    resize(img,img1,Size(700,700)); 
+    pyrUp(img1,imgd,Size(img1.cols *2,img1.rows *2));
+    pyrDown(img1,imgd1,Size(img1.cols/2, img1.rows/2));
+    imshow("img",img1);
+    imshow("imgd", imgd);
+    imshow("imgd1",imgd1);
+    waitKey(0);
+    return 0;
+}
